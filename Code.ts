a986e4032569ef1f943226f1496ef94dc1d02569
appare45 <kaibatsu35.7m45@gmail.com>
2021-02-14 00:36:08 +0900

    Typescript用の型を追加

--- Code.ts
+++ Code.ts
@@ -3,6 +3,27 @@ const mailBody: string = "This is a test message from GAS";
 
 const SpreadSheetID: string = "1axwEK-8ozcXGm5Xux2A83xYV5pi7aQUv8eHsbIlj8gI";
 
+interface slackEventResponse {
+  // https://api.slack.com/apis/connections/events-api#the-events-api__receiving-events__callback-field-overview
+  token: string;
+  team_id: string;
+  api_spp_id: string;
+  type: string;
+  authed_users?: [];
+  authed_teams?: [];
+  authorizations: Object;
+  event_id: string;
+  event_time: number;
+  event: {
+    type: "app_mention";
+    user: string;
+    text: string;
+    ts: string;
+    channel: string;
+    event_ts: string;
+  };
+}
+
 function getEmailList(
   SpreadSheetID: string
 ): { address: string; name: string }[] {
@@ -25,21 +46,34 @@ function SendEmail(recipient: string, subject: string, body: string) {
   });
 }
 
-function doPost(e) {
+type slackEventResponseReadOnly = Readonly<slackEventResponse>;
+
+function appMentioned(slackData: slackEventResponse) {
+  // https://api.slack.com/events/app_mention
   const emailList = getEmailList(SpreadSheetID);
-  const slackData = JSON.parse(e.postData.getDataAsString());
+
   const postedUser: string = getUserInfo(slackData.event.user).ok
     ? getUserInfo(slackData.event.user).profile.display_name_normalized != ""
       ? getUserInfo(slackData.event.user).profile.display_name_normalized
       : getUserInfo(slackData.event.user).profile.real_name_normalized
     : "unknown";
-  emailList.forEach((emailData) => {
+  emailList.forEach((emailData: { address: string; name: string }) => {
     SendEmail(
       emailData.address,
       `A message from ${postedUser}`,
       `${postedUser}: ${slackData.event.text}`
     );
   });
+}
+
+function doPost(e) {
+  const slackData: slackEventResponse = JSON.parse(
+    e.postData.getDataAsString()
+  );
+  if (slackData.type == "app_mention") {
+    appMentioned(slackData);
+  }
+
   const response = {
     challenge: e,
   };
